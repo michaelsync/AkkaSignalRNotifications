e735060c1d6bf2c5fb76cff60c57aa079c7d9724
michaelsync <mchlsync@gmail.com>
2015-09-17 12:59:31 +1000

    moved the code related to notification

---
+++ AkkaSignalRNotifications/Actors/NotificationActor.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using Akka.Actor;
+using AkkaSignalRNotifications.Messages;
+
+namespace AkkaSignalRNotifications.Actors
+{
+
+    public class NotificationActor : ReceiveActor
+    {
+        private readonly HashSet<LogSubscriber> _logSubscribers = new HashSet<LogSubscriber>();
+        private readonly HashSet<IActorRef> _progressSubscribers = new HashSet<IActorRef>();
+
+        public HashSet<LogSubscriber> LogSubscribers
+        {
+            get
+            {
+                return _logSubscribers;
+            }
+        }
+
+        public HashSet<IActorRef> ProgressSubscribers
+        {
+            get
+            {
+                return _progressSubscribers;
+            }
+        }
+
+        public NotificationActor()
+        {
+            Receive<SubscribeMessage>(m =>
+            {
+                if (m.InterestedIn.HasFlag(NotificationTypes.Log))
+                {
+                    _logSubscribers.Add(new LogSubscriber(m.Subscriber, m.LogLevel));
+                }
+
+                if (m.InterestedIn.HasFlag(NotificationTypes.Progress))
+                {
+                    _progressSubscribers.Add(m.Subscriber);
+                }
+            });
+
+            Receive<UnsubscribeMessage>(m =>
+            {
+                _logSubscribers.RemoveWhere(x => x.Subscriber.Equals(m.Subscriber));
+                _progressSubscribers.Remove(m.Subscriber);
+            });
+
+            Receive<ProgressSnapshotMessage>(m =>
+            {
+                foreach (IActorRef sub in _progressSubscribers)
+                {
+                    sub.Tell(m);
+                }
+            });
+
+            Receive<LogMessage>(m =>
+            {
+                foreach (LogSubscriber sub in _logSubscribers)
+                {
+                    if (m.Level >= sub.InterestedLevel)
+                    {
+                        sub.Subscriber.Tell(m);
+                    }
+                }
+            });
+        }
+
+        public class LogSubscriber : IEqualityComparer<LogSubscriber>
+        {
+            public LogLevel InterestedLevel { get; private set; }
+            public IActorRef Subscriber { get; private set; }
+
+            public LogSubscriber(IActorRef subscriber, LogLevel interestedLevel)
+            {
+                Subscriber = subscriber;
+                InterestedLevel = interestedLevel;
+            }
+
+            public bool Equals(LogSubscriber x, LogSubscriber y)
+            {
+                return x.Subscriber.Equals(y.Subscriber);
+            }
+
+            public int GetHashCode(LogSubscriber x)
+            {
+                return x.Subscriber.GetHashCode();
+            }
+        }
+    }
+}
---
+++ AkkaSignalRNotifications/Messages/NotificationMessages.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Akka.Actor;
+
+namespace AkkaSignalRNotifications.Messages
+{
+
+    public enum JobExecutionStatus
+    {
+        BuildingWorkspace,
+        Processing,
+        Succeeded,
+        Failed
+    }
+
+
+    public class ProgressStageSnapshot
+    {
+        public long Succeeded { get; private set; }
+        public long Failed { get; private set; }
+
+        public long Total
+        {
+            get
+            {
+                return Succeeded + Failed;
+            }
+        }
+
+        public ProgressStageSnapshot(long succeeded, long failed)
+        {
+            Succeeded = succeeded;
+            Failed = failed;
+        }
+    }
+
+    public class ProgressSnapshotMessage
+    {
+        public JobExecutionStatus Status { get; private set; }
+        public ProgressStageSnapshot Consumed { get; private set; }
+        public ProgressStageSnapshot Serialised { get; private set; }
+        public long TotalEvents { get; private set; }
+        public bool IsStillCalculatingTotalEvents { get; private set; }
+
+        public ProgressSnapshotMessage(JobExecutionStatus status, ProgressStageSnapshot consumed, ProgressStageSnapshot serialised, long totalEvents, bool isCalculatingTotalEvents)
+        {
+            Status = status;
+            Consumed = consumed;
+            Serialised = serialised;
+            TotalEvents = totalEvents;
+            IsStillCalculatingTotalEvents = isCalculatingTotalEvents;
+        }
+    }
+
+    [Flags]
+    public enum NotificationTypes
+    {
+        None = 0,
+        Log = 1,
+        Progress = 2
+    }
+
+    public enum LogLevel
+    {
+        Verbose,
+        Debug,
+        Information,
+        Warning,
+        Error,
+        Fatal,
+        None
+    }
+
+    public class LogMessage
+    {
+        public LogLevel Level { get; private set; }
+        public string Message { get; private set; }
+
+        public LogMessage(LogLevel level, string message)
+        {
+            Level = level;
+            Message = message;
+        }
+    }
+
+    public class SubscribeMessage
+    {
+        public NotificationTypes InterestedIn { get; private set; }
+        public LogLevel LogLevel { get; private set; }
+        public IActorRef Subscriber { get; private set; }
+
+        public SubscribeMessage(IActorRef subscriber, NotificationTypes interestedIn)
+            : this(subscriber, interestedIn, LogLevel.None)
+        {
+
+        }
+
+        public SubscribeMessage(IActorRef subscriber, NotificationTypes interestedIn, LogLevel logLevel)
+        {
+            Subscriber = subscriber;
+            InterestedIn = interestedIn;
+            LogLevel = logLevel;
+        }
+    }
+
+    public class UnsubscribeMessage
+    {
+        public IActorRef Subscriber { get; private set; }
+
+        public UnsubscribeMessage(IActorRef subscriber)
+        {
+            Subscriber = subscriber;
+        }
+    }
+}
