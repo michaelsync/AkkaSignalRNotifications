8a5b917673b7ab21d4d2d0dd1cc72a736e8f4123
michaelsync <mchlsync@gmail.com>
2015-09-17 14:27:20 +1000

    added a test code for client as well

--- AkkaSignalRNotifications/App_Start/BundleConfig.cs
+++ AkkaSignalRNotifications/App_Start/BundleConfig.cs
@@ -23,9 +23,15 @@ namespace AkkaSignalRNotifications
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/signalr").Include("~/Scripts/jquery.signalR-{version}.js")
+              .Include("~/Scripts/knockout-{version}.js")
+              .Include("~/Scripts/date.format.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
