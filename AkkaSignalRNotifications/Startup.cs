1d61694301228f0d605583a648d3a1fd458df6db
michaelsync <mchlsync@gmail.com>
2015-09-17 13:06:26 +1000

    removed unnecessary files

--- AkkaSignalRNotifications/Startup.cs
+++ AkkaSignalRNotifications/Startup.cs
@@ -8,7 +8,7 @@ namespace AkkaSignalRNotifications
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            app.MapSignalR();
         }
     }
 }
